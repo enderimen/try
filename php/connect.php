660df9d2b3223f915edebd78cecbd24f863f2c0c
Ender İMEN <enderimen@hotmail.com>
2017-10-14 09:28:32 +0300

    database connectivity provided.

---
+++ php/connect.php
@@ -0,0 +1,16 @@
+<?php 
+
+	$localhost	=	"localhost";
+	$username	=	"root";
+	$password	=	"";
+	$DBname	 	=	"tanitim";
+
+	$connect = mysqli_connect($localhost, $username, $password, $DBname);
+
+	if ($connect) {
+		echo "Bağlantı Sağlandı.";
+	}else{
+		echo "no connect";
+	}
+
+ ?>
